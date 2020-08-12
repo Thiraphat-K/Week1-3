f52ec04d8409e95393f5f35dc806e17d15844a5c
Thiraphat Ketsingnoi <63010467@kmitl.ac.th>
2020-08-12 23:13:53 +0700

    input,output

---
+++ Week1-3/Source.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int M = 630 , S = 10, N = 467;
+	printf("\nM = 630\n");
+	printf("\nS = 10\n");
+	printf("\nN = 467\n");
+	printf("\nMy Student Number : %d%d%d\n", M, S, N);
+	return 0;
+}
